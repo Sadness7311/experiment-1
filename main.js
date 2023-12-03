2d8cb20249b867cb90f30e47c1f170a3155647ed
UmairSaeed <123866496+Sadness7311@users.noreply.github.com>
2023-12-03 23:52:27 +0500

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const world = new World(document.querySelector('.webgl'))
 const { scene, camera, renderer } = world
 
 
-const h11 = create3d2dElem('h1', "I'm Sadness7311, I'm")
-const h12 = create3d2dElem('h1', "a Web Developer.")
+const h11 = create3d2dElem('h1', "Experiment 1.0, It's")
+const h12 = create3d2dElem('h1', "A Space Site")
 h11.position.y = 200
 h12.position.y = 50
 scene.add(h11, h12)
